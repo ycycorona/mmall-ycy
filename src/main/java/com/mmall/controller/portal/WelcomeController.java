6a4ac6725a926a831d02ee085a91eab8313d0b36
QDAIRLINES\yichenyang <yichenyang@qdairlines.com>
2018-03-10 16:39:14 +0800

    work pc 初始化一波

---
+++ src/main/java/com/mmall/controller/portal/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by yichenyang on 2018/3/10.
+ */
+@Controller
+@RequestMapping("/welcome/")
+public class WelcomeController {
+    @RequestMapping(value = "index")
+    public String welcome(){
+        return "/index";
+    }
+}
